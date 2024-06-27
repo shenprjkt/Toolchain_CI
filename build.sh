6733bae2f72bc8cad02adac5e4f480024d3bdcb9
Itz-Kanasama <164613153+Itz-Kanasama@users.noreply.github.com>
2024-06-27 16:50:42 +0700

    ClangCI: Update Base ci.sh

--- build.sh
+++ build.sh
@@ -3,80 +3,6 @@
 git clone https://github.com/Itz-Kanasama/llvmTC $(pwd)/llvmTC -b llvm-tc
 cd $(pwd)/llvmTC
 
-BASE=$(dirname "$(readlink -f "${0}")")
-
-set -eu
-
-function parse_parameters() {
-    while ((${#})); do
-        case ${1} in
-            all | binutils | deps | kernel | llvm) ACTION=${1} ;;
-            *) exit 33 ;;
-        esac
-        shift
-    done
-}
-
-function do_all() {
-    do_deps
-    do_llvm
-    do_binutils
-    do_kernel
-}
-
-function do_binutils() {
-    "${BASE}"/build-binutils.py -t x86_64
-}
-
-function do_deps() {
-    # We only run this when running on GitHub Actions
-    [[ -z ${GITHUB_ACTIONS:-} ]] && return 0
-    sudo apt-get install -y --no-install-recommends \
-        bc \
-        bison \
-        ca-certificates \
-        clang \
-        cmake \
-        curl \
-        file \
-        flex \
-        gcc \
-        g++ \
-        git \
-        libelf-dev \
-        libssl-dev \
-        lld \
-        make \
-        ninja-build \
-        python3 \
-        texinfo \
-        xz-utils \
-        zlib1g-dev
-}
-
-function do_kernel() {
-    cd "${BASE}"/kernel
-    ./build.sh -t X86
-}
-
-function do_llvm() {
-    EXTRA_ARGS=()
-    [[ -n ${GITHUB_ACTIONS:-} ]] && EXTRA_ARGS+=(--no-ccache)
-    "${BASE}"/build-llvm.py \
-        --assertions \
-        --branch "release/13.x" \
-        --build-stage1-only \
-        --check-targets clang lld llvm \
-        --install-stage1-only \
-        --projects "clang;lld" \
-        --shallow-clone \
-        --targets X86 \
-        "${EXTRA_ARGS[@]}"
-}
-
-parse_parameters "${@}"
-do_"${ACTION:=all}"
-
 # Function to show an informational message
 msg() {
     echo -e "\e[1;32m$*\e[0m"
@@ -105,12 +31,13 @@ if [[ ! -z "${2}" ]];then
     TomTal=$(($TomTal*2))
 fi
 ./build-llvm.py \
-	--clang-vendor "$LLVM_NAME" \
-	--targets "ARM;AArch64" \
 	--defines "LLVM_PARALLEL_COMPILE_JOBS=$TomTal LLVM_PARALLEL_LINK_JOBS=$TomTal CMAKE_C_FLAGS='-g0 -O3' CMAKE_CXX_FLAGS='-g0 -O3'" \
+	--branch "release/13.x" \
+  	--build-stage1-only \
+	--targets "ARM;AArch64" \
 	--shallow-clone \
 	--no-ccache \
-	--branch "main" 2>&1 | tee build.log
+ 	--clang-vendor "$LLVM_NAME" 2>&1 | tee build.log
 
 # Check if the final clang binary exists or not.
 [ ! -f install/bin/clang-1* ] && {
