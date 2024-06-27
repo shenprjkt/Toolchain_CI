31800f85a07592b53d1eb9d0aa880adc6bfa8896
Itz-Kanasama <164613153+Itz-Kanasama@users.noreply.github.com>
2024-06-27 10:19:39 +0000

    ClangCI: Update Latest TC_BUILD

--- build.sh
+++ build.sh
@@ -1,43 +1,46 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/Itz-Kanasama/llvmTC $(pwd)/llvmTC -b llvm-tc
+git clone https://github.com/Redmi-S2-Y2-Resources/tc-build $(pwd)/llvmTC -b main
 cd $(pwd)/llvmTC
 
 # Function to show an informational message
 msg() {
     echo -e "\e[1;32m$*\e[0m"
 }
-
 err() {
-    echo -e "\e[1;41m$*\e[0m"
+    echo -e "\e[1;41$*\e[0m"
 }
 
 GITHUB_TOKEN=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
 LLVM_NAME=Paradise
 
-# Set a directory
+# Environment Config
+export BRANCH=main
+export CACHE=1
+
+# Get home directory
 DIR="$(pwd ...)"
+install=$DIR/install
+src=$DIR/src
 
-# Build Info
-rel_date="$(date "+%Y%m%d")" # ISO 8601 format
-rel_friendly_date="$(date "+%B %-d, %Y")" # "Month day, year" format
-builder_commit="$(git rev-parse HEAD)"
-
-# Build LLVM
-msg "$LLVM_NAME: Building LLVM..."
-tg_post_msg "<b>$LLVM_NAME: Building LLVM. . .</b>"
-TomTal=$(nproc)
-if [[ ! -z "${2}" ]];then
-    TomTal=$(($TomTal*2))
-fi
+# Building LLVM's
+msg "Building LLVM's ..."
+chmod +x build-llvm.py
 ./build-llvm.py \
-	--defines "LLVM_PARALLEL_COMPILE_JOBS=$TomTal LLVM_PARALLEL_LINK_JOBS=$TomTal CMAKE_C_FLAGS='-g0 -O3' CMAKE_CXX_FLAGS='-g0 -O3'" \
-	--branch "release/13.x" \
-  	--build-stage1-only \
-	--targets "ARM;AArch64" \
-	--shallow-clone \
-	--no-ccache \
- 	--clang-vendor "$LLVM_NAME" 2>&1 | tee build.log
+    --defines LLVM_PARALLEL_COMPILE_JOBS="$(nproc)" LLVM_PARALLEL_LINK_JOBS="$(nproc)" CMAKE_C_FLAGS=-O3 CMAKE_CXX_FLAGS=-O3 \
+    --install-folder "$install" \
+    --assertions \
+    --build-stage1-only \
+    --build-target distribution \
+    --check-targets clang lld llvm \
+    --install-target distribution \
+    --projects all \
+    --quiet-cmake \
+    --shallow-clone \
+    --show-build-commands \
+    --targets ARM AArch64 X86 \
+    --ref "release/18.x" \
+    --vendor-string "$LLVM_NAME" 2>&1 | tee build.log
 
 # Check if the final clang binary exists or not.
 [ ! -f install/bin/clang-1* ] && {
@@ -47,25 +50,24 @@ fi
 }
 
 # Build binutils
-msg "$LLVM_NAME: Building binutils..."
-./build-binutils.py --targets arm aarch64
+msg "Build binutils ..."
+chmod +x build-binutils.py
+./build-binutils.py \
+    --install-folder "$install" \
+    --targets arm aarch64 x86_64
 
-# Remove unused products
 rm -fr install/include
 rm -f install/lib/*.a install/lib/*.la
 
-# Strip remaining products
 for f in $(find install -type f -exec file {} \; | grep 'not stripped' | awk '{print $1}'); do
-	strip -s "${f: : -1}"
+    strip -s "${f::-1}"
 done
 
-# Set executable rpaths so setting LD_LIBRARY_PATH isn't necessary
 for bin in $(find install -mindepth 2 -maxdepth 3 -type f -exec file {} \; | grep 'ELF .* interpreter' | awk '{print $1}'); do
-	# Remove last character from file output (':')
-	bin="${bin: : -1}"
+    bin="${bin::-1}"
 
-	echo "$bin"
-	patchelf --set-rpath "$DIR/install/lib" "$bin"
+    echo "$bin"
+    patchelf --set-rpath "$DIR/../lib" "$bin"
 done
 
 # Release Info
@@ -92,6 +94,6 @@ git commit -asm "Paradise: Update to $rel_date build
 LLVM commit: $llvm_commit_url
 Clang Version: $clang_version
 Binutils version: $binutils_ver
-Builder commit: https://github.com/Itz-Kanasama/llvmTC/commit/$builder_commit"
+Builder commit: https://github.com/Redmi-S2-Y2-Resources/tc-build/commit/$builder_commit"
 git push -f
 popd || exit
